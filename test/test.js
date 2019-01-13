eb79ea49f4294fc1a133dd8fc45780f30f787916
gjc9620 <992758355@qq.com>
2019-01-13 18:51:42 +0800

    test

--- test/test.js
+++ test/test.js
@@ -1,108 +1,54 @@
-// var format = require("string-template");
-// var template = require("../index");
-//
-// console.log(
-//   format("Hello {{name}, you have {count} {vvv} {{aaa}} unread messages", {
-//     name: "Robert",
-//     count: 12
-//   })
-// );
-//
-// template(
-//   {
-//     str: "Hello {{name}, you have {count} {vvv} {{aaa}} unread messages",
-//     values: {
-//       name: "Robert",
-//       count: 12
-//     },
-//     render: (arr)=>console.log(arr.join('')),
-//   }
-// );
-//
-//
-// ///////////////////////////////////////
-//
-// console.log(
-//   format("111{name}1111", {
-//     name: "Robert",
-//     count: 12
-//   })
-// );
-//
-// template(
-//   {
-//     str: "111{name}1111",
-//     values: {
-//       name: "Robert",
-//       count: 12
-//     },
-//     render: (arr)=>console.log(arr.join('')),
-//   }
-// );
-//
-//
-// ///////////////////////////////////
-//
-// console.log(
-//   format("1111111", {
-//     name: "Robert",
-//     count: 12
-//   })
-// );
-//
-// template(
-//   {
-//     str: "1111111",
-//     values: {
-//       name: "Robert",
-//       count: 12
-//     },
-//     render: (arr)=>console.log(arr.join('')),
-//   }
-// );
-//
-//
-// /////////////////////////////////// no match
-//
-//
-// template(
-//   {
-//     str: "111{ggg}1111",
-//     renderNoMatch: ()=>'AAAA',
-//     values: {
-//       name: "Robert",
-//       count: 12
-//     },
-//     render: (arr)=>console.log(arr.join('')),
-//   }
-// );
-//
-// template(
-//   {
-//     str: "111{ggg}1111",
-//     renderNoMatch: ()=>'{}{}{}}{}{{{}}}{AAA}',
-//     values: {
-//       name: "Robert",
-//       count: 12
-//     },
-//     render: (arr)=>console.log(arr.join('')),
-//   }
-// );
-//
-//
-// ///////////////////////////////////
-//
-//
-// template(
-//   {
-//     str: "111{name}11{count}11{bad}ddddddd{sym}",
-//     renderNoMatch: ()=>'{}{}{}}{}{{{}}}{AAA}',
-//     values: {
-//       name: function () {},
-//       count: { type: 1},
-//       bad: undefined,
-//       sym: Symbol("A"),
-//     },
-//     render: (arr)=>console.log(arr),
-//   }
-// );
+import React from 'react';
+import ReactStringTemplate from '../lib';
+import renderer from 'react-test-renderer';
+
+test('With React element', () => {
+  const component = renderer.create(
+    <ReactStringTemplate
+      str={"{name} is a series of {foo} novels written by British {{author}} {author} in {yeah}."}
+      renderNoMatch={()=>'not match'}
+      values={{
+        name: <a href="https://www.google.com/search?q=Harry Potter" key='potter'>Harry Potter</a>,
+        foo: 'fantasy',
+        bar: 'novels',
+        author: 'J. K. Rowling',
+        yeah: 1997,
+      }} >
+      {(children)=><div>{children}</div>}
+    </ReactStringTemplate>,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Insert React element and click event', () => {
+  const component = renderer.create(
+    <ReactStringTemplate
+      str={"{charmName} Charm"}
+      values={{
+        charmName: (
+          <span
+            key='patronus'
+            onClick={()=>alert('Patronus!')}>
+            Patronus
+          </span>
+        ),
+      }} >
+      {(children)=><div>{children}</div>}
+    </ReactStringTemplate>,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('If not match', () => {
+  const component = renderer.create(
+    <ReactStringTemplate
+      str={"I am {name}"}
+      renderNoMatch={()=>'not match'} >
+      {(children)=><div>{children}</div>}
+    </ReactStringTemplate>,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
